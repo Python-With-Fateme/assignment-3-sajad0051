97fc91bd648f8bfe73da2c312625b252cd93c081
sajjad <sajjadmahmodi051@gmail.com>
2022-09-03 21:22:59 +0430

    assignment3

--- as3-2.py
+++ as3-2.py
@@ -1,10 +1,4 @@
-for i in range (1):
-    print('*****')
-for i in range(1):
-    print ('****')
-for i in range (1):
-    print('***')
-for i in range(1):
-    print ('**')
-for i in range (1):
-    print ('*')
+for i in range (6):
+    for i in range(6,i,-1):
+        print('*',end=' ')
+    print()
